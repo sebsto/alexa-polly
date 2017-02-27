52895daabd8c378b449442513f1a08bed4921996
Sebastien Stormacq <sebastien.stormacq@gmail.com>
2017-02-27 07:33:03 +0100

    Update with comments

--- package.sh
+++ package.sh
@@ -1,6 +1,16 @@
+# The bucket where to upload Polly's MP3
+#
+# To use this bucket as an URL that can be consumed by Alexa,
+# you have to configure the bucket to serve web content
+# you also need to have a valid and recognized SSL certificate 
+# <bucket_name>.s3.amaonaws.com is not recognized by browsers,
+# So I am using a cloudfront distribution, pointing to the bucket 
+# and using a AWS Certificate associated to it
+S3_BUCKET=alexademo.ninja
+
 aws cloudformation package \
    --template-file alexa-sa-polly.yaml \
    --output-template-file serverless-output.yaml \
-   --s3-bucket alexademo.ninja \
+   --s3-bucket $S3_BUCKET \
    --s3-prefix alexa-sa-polly
 
